24192fdb4f11cb0f1307c2a6cb103a1b422960ec
Pietromantov <pietro.mantovani@studenti.unitn.it>
2026-02-18 18:48:12 +0100

    tockenChecker: tolti i console.log

--- src/app/tokenChecker.js
+++ src/app/tokenChecker.js
@@ -2,7 +2,6 @@ import jwt from 'jsonwebtoken';
 
 const tokenChecker = function(req, res, next) {
 	var token = req.query.token || req.headers['x-access-token'];
-	console.log('tokenchecker= ',token); //da togliere
 	if (!token) {
 		return res.status(401).send({ 
 			success: false,
@@ -11,16 +10,13 @@ const tokenChecker = function(req, res, next) {
 	}
 
 	jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {			
-		console.log('decoded= ',decoded); //da togliere
 		if(err){
-			console.log('errore: ',err); //ad togliere
 			return res.status(403).send({
 				success: false,
 				message: 'Failed to authenticate token.'
 			});		
 		}
         else{
-			console.log('token verificato'); //da togliere
 			req.loggedUser = decoded;
 			next();
 		}
