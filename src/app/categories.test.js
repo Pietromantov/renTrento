c61aebbf614c5a2d01209837e1169d9161210ad5
Radu E <gia.rad15@gmail.com>
2026-02-19 17:30:04 +0100

    Ulteriore setup per testing

---
+++ src/app/categories.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('./app');
+
+/*
+test('get', () => {
+    expect.assertions(1);
+
+    fetch("/categories")
+    .then( res=> {
+        expect( res.status ).toEqual(200)
+    })
+
+});*/
