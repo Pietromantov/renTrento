a7bf71d9eabb97ed09ff3b5f5920fe3ed251926d
Radu E <gia.rad15@gmail.com>
2026-02-19 18:11:22 +0100

    primi file di testing

--- src/app/app.test.js
+++ src/app/app.test.js
@@ -1,12 +1,6 @@
-const request = require('supertest');
-const app = require('./app');
+import request from 'supertest';
+import app from './app.js';
 
 test('app module should be defined', () => {
   expect(app).toBeDefined();
-});
-
-test('GET / should return 200', () => {
-  return request(app)
-    .get('/')
-    .expect(200);
 });
